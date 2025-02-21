fdc4581c110ef3441511539516f971e96efa5912
Ramakrishna Mohan <Ramakrishna Mohan@DESKTOP-0G3DS1U>
2025-02-21 10:51:20 +0530

    Add project files.

---
+++ Event_Track_App/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Event_Track_App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Event_Track_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Buttons()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Event_Track_App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Event_Track_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
